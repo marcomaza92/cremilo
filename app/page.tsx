59b1fc582c965827993229448755862a9ab8ed39
marcomaza92 <marcoagustinmaza@gmail.com>
2025-01-02 23:55:10 -0300

    feat: add supabase for server and client

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import styles from './page.module.css'
+import { createClient } from "@/utils/supabase/server";
+import styles from "./page.module.css";
 
-const Home = () => {
-  return (
-    <h1 className='test'>Crémilo</h1>
-  )
-}
+const Home = async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("transactions").select();
+
+  console.log(data);
+  console.log(error);
+
+  return <h1 className="test">Crémilo</h1>;
+};
 
 export default Home;
