a15e3d7aee71bdd424f0ca27c1f24dcc3b421958
marcomaza92 <marcoagustinmaza@gmail.com>
2025-01-03 23:08:58 -0300

    feat: add nested routing with custom layouts

---
+++ app/(main-layout)/about/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./page.module.css";
+
+const About = async () => {
+  return <h1 className="color-base-turquoise">About</h1>;
+};
+
+export default About;
---
+++ app/(main-layout)/layout.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "./layout.module.css";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href={"/"}>Homepage</Link>
+            </li>
+            <li>
+              <Link href={"/about"}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>This is the footer</footer>
+    </>
+  );
+};
+
+export default MainLayout;
